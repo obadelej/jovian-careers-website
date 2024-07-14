ed729788823ba5aa49a8975dd002d6981518956f
obadelej <obadelej@gmail.com>
2024-07-14 13:33:56 +0000

    added dynamic data
    Added a python list with dynamic api route of persons details

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
+Persons = [
+    {    
+        'id': 1,
+        'name': 'Taigon',
+        'age': '19',
+        'address': 'Willis'
+    },
+    
+    {    
+        'id': 2,
+        'name': 'Ethan',
+        'age': '17',
+        'address': 'La Mode'
+    },
+    {    
+        'id': 3,
+        'name': 'Devonni',
+        'age': '19',
+        'address': 'Gouyave'
+    }
+        
+]
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', persons=Persons)
+
+
+@app.route("/api/persons")
+def list_persons():
+    return jsonify(Persons)
+    
   
 
 if __name__ == "__main__":
